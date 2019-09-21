f23b46a798b32c17292692a35c135c41ee9b4862
David A Roberts <d@vidr.cc>
2019-09-21 19:14:52 +1000

    fix link error

--- txd.cc
+++ txd.cc
@@ -161,3 +161,24 @@ std::vector<Texture> loadTXD(const std::string &s) {
 std::string dataURI(const Texture &texture) {
     return "data:image/png;base64," + base64_encode(texture.png.data(), texture.png.size());
 }
+
+#ifdef __EMSCRIPTEN__
+#include <emscripten/bind.h>
+using namespace emscripten;
+EMSCRIPTEN_BINDINGS(txd) {
+    value_object<Texture>("Texture")
+        .field("name",        &Texture::name)
+        .field("alpha",       &Texture::alpha)
+        .field("png",         &Texture::png)
+        .field("transparent", &Texture::transparent)
+        .field("minFilter",   &Texture::minFilter)
+        .field("magFilter",   &Texture::magFilter)
+        .field("wrapS",       &Texture::wrapS)
+        .field("wrapT",       &Texture::wrapT)
+        ;
+    register_vector<unsigned char>("Buffer");
+    register_vector<Texture>("TextureList");
+    function("loadTXD", select_overload<std::vector<Texture>(const std::string &)>(&loadTXD));
+    function("dataURI", &dataURI);
+}
+#endif
